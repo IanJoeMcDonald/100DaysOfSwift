ea56ce4b3a5057310f734a51c4d57212ac30eced
Ian McDonald <ianjoemcdonald@gmail.com>
2020-03-09 10:53:00 -0300

    Project 16: Challenge 1 Complete

--- Project16/Project16/Project16/ViewController.swift
+++ Project16/Project16/Project16/ViewController.swift
@@ -33,12 +33,13 @@ class ViewController: UIViewController, MKMapViewDelegate {
         let identifier = "Capital"
 
         // 3
-        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier) as? MKPinAnnotationView
 
         if annotationView == nil {
             //4
             annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
             annotationView?.canShowCallout = true
+            annotationView?.pinTintColor = UIColor.green
 
             // 5
             let btn = UIButton(type: .detailDisclosure)
